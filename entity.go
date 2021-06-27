c8f1ae0a51ef1e641d277ccd442f0dc71b0c1b42
Jordy Kristianata <jordykristianata@gmail.com>
2021-06-27 12:17:52 +0700

    Introduction

---
+++ entity.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Test() string {
+	return fmt.Sprintf("Halo, nama saya %s dan umur saya %d", "Jordy", 22)
+}
--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// import (
+// 	"fmt" 
+// 	"golang-first-project/calculation"
+// ) 
+
 func main() {
-	fmt.Println("Halo, belajar golang")
+	var name string = "Golang"
+	fmt.Println(name)
+	// section 1
+	// fmt.Println("Halo, belajar golang")
+	// sentence := Test()
+	// fmt.Println(sentence)
+	// result := calculation.Add(10,12)
+	// result := calculation.Multiply(10,12)
+	// fmt.Println(result)
 }
